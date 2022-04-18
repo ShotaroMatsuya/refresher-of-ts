45fe7eab3855b75db0317c5fb97bb259ddd4334d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-18 16:25:15 +0900

    learning any & union type

--- app/basics.ts
+++ app/basics.ts
@@ -2,7 +2,9 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-const number1 = 5;
+let number1: number;
+
+number1 = 5;
 const number2 = 2.8;
 
 const result = add(number1, number2);
