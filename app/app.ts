fe0867ae44d50616cd8c7cd9bdad936c656416d8
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-18 16:02:30 +0900

    learning tuple type

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,31 @@
-console.log('Your code goes here...');
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Showtarou',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
+};
+
+// person.role.push('admin');
+// person.role = [0, 'admin', 'user'];
+
+// let favoriteActivities: any[];
+// favoriteActivities = ['Sports', 1];
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!
+}
