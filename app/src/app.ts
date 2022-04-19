be4b83a8165982df26aabe6b193c0f879f57b2df
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-19 15:45:10 +0900

    arr & obj desctructuring

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,9 +1,9 @@
-const userName = 'Show';
-// userName = 'ShowTarou';
-let age = 30;
+// const userName = 'Show';
+// // userName = 'ShowTarou';
+// let age = 30;
 
-age = 29;
-var result;
+// age = 29;
+// var result;
 
 // function add(a: number, b: number) {
 //   result = a + b;
@@ -41,7 +41,7 @@ const activeHobbies = ['Hiking', ...hobbies];
 activeHobbies.push(...hobbies);
 
 const person = {
-  name: 'Show',
+  firstName: 'Show',
   age: 30,
 };
 
@@ -56,3 +56,13 @@ const add = (...numbers: number[]) => {
 const addedNumbers = add(5, 10, 2, 3.7);
 
 console.log(addedNumbers);
+
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
