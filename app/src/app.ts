10bd39e7743b6304550b8a06e2361d4c71e8840d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-19 15:11:36 +0900

    diffrence between let and var

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+const userName = 'Show';
+// userName = 'ShowTarou';
+let age = 30;
+
+age = 29;
+var result;
+
+function add(a: number, b: number) {
+  result = a + b;
+  return result;
+}
+
+// this works
+// if (age > 20) {
+//   var isOld = true;
+// }
+
+// this not work
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
