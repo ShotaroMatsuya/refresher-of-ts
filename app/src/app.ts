82f207a0c222b7819efee187f55c2aa4995b4987
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-19 15:33:08 +0900

    rest parameters

--- app/src/app.ts
+++ app/src/app.ts
@@ -22,18 +22,18 @@ var result;
 
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
-console.log(add(2, 5));
+// const add = (a: number, b: number = 1) => a + b;
+// console.log(add(2, 5));
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-  button.addEventListener('click', event => console.log(event));
-}
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking', ...hobbies];
@@ -46,3 +46,13 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+
+console.log(addedNumbers);
