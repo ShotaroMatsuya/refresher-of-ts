c72313a8d42439092d8fb23cf0e6c85f39487ae1
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-20 15:09:22 +0900

    public / private

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  private name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -7,12 +8,24 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('user1');
+accounting.addEmployee('user2');
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
