9ccf1d75a3e138db4e0b2218997c42d67643c284
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-20 15:45:00 +0900

    overwrite & protected props

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -32,6 +32,14 @@ class AccountDepartment extends Department {
   constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
   }
+
+  addEmployee(name: string) {
+    if (name === 'userX') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -53,7 +61,11 @@ console.log(it);
 const accounting = new AccountDepartment('d2', []);
 accounting.addReport('Something went wrong...');
 
+accounting.addEmployee('userX');
+accounting.addEmployee('user3');
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
