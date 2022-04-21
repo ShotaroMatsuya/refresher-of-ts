15b418368fc51d75ada15257f058d18a3ca1c232
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-21 15:07:59 +0900

    using keyof constraint

--- app/src/app.ts
+++ app/src/app.ts
@@ -39,4 +39,13 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   return [element, descriptionText];
 }
 
-console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+console.log(extractAndConvert({ name: 'Max' }, 'name'));
