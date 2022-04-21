c79503bb19be0998a79cde5a14c9b8227e3ab268
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-21 14:46:14 +0900

    creating a Generic Function

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+// const names: Array<string> = ['User1', 'User2'];
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   // data.split(' ');
+// });
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+
+// use Generic types
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({ name: 'Max' }, { age: 30 }));
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+// console.log(mergedObj.name); //Property 'name' does not exist on type 'object'.
+console.log(mergedObj.name);
