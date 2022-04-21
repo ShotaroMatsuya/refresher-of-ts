bd2d810afb3b0b2402c5a4b93f195d89ec69dccd
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-21 15:34:22 +0900

    generic classes with primitive types

--- app/src/app.ts
+++ app/src/app.ts
@@ -49,3 +49,35 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: 'Max' }, 'name'));
+
+// primitive typeでは有用(arrayやobjでは使わないほうが良い)
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('User1');
+textStorage.addItem('User2');
+textStorage.removeItem('User1');
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const user1Obj = { name: 'user1' };
+// objStorage.addItem(user1Obj);
+// objStorage.addItem({ name: 'user2' });
+
+// objStorage.removeItem(user1Obj); // this obj is technically a brand new object in a memory and has a different address
+// console.log(objStorage.getItems());
