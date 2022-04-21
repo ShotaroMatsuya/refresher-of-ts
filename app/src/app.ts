6293ffa1653c541a8d9e3507cc476f1a049b5e89
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-21 13:02:58 +0900

    optional chaning

--- app/src/app.ts
+++ app/src/app.ts
@@ -37,6 +37,13 @@ function add(a: Combinable, b: Combinable) {
 const result = add('M1', 'S1');
 result.split(' ');
 
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Max',
+  job: { title: 'CEO', description: 'My own company' },
+};
+console.log(fetchedUserData?.job?.title);
+
 // type UnknownEmployee = Employee | Admin;
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
