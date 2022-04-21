e524616e791a839578202426638c3b91d21e0f77
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-21 15:02:12 +0900

    generic with interface

--- app/src/app.ts
+++ app/src/app.ts
@@ -24,3 +24,19 @@ console.log(merge({ name: 'Max' }, { age: 30 }));
 const mergedObj = merge({ name: 'Max', hobbies: ['sports'] }, { age: 30 });
 // console.log(mergedObj.name); //Property 'name' does not exist on type 'object'.
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
