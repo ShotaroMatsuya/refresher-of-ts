3db50799e380781a68cc6957026aadee33414d38
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-22 15:03:08 +0900

    when property decorator execute ?

--- app/src/app.ts
+++ app/src/app.ts
@@ -3,6 +3,7 @@
 //   console.log(constructor);
 // }
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -10,7 +11,9 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
   return function (constructor: any) {
+    console.log('Rendering template');
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -33,3 +36,32 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+//
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!');
+  console.log(target, propertyName);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
