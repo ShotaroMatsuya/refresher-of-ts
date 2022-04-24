5803bb89ee63468410f37976d586a25caca4f8f0
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-24 20:42:47 +0900

    oop rendering

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    // importNode...Nodeエレメントを複製する(recursiveに)
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    // insertAdjacentElement...自身を基準とした相対的な位置に要素を挿入
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
