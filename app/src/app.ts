9517ed959a09c51f700cce6d105b3ca8068ff5b2
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-25 00:50:50 +0900

    filtered status of prjs

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,5 @@
 // Project Type
+//グローバル定数として使える
 enum ProjectStatus {
   Active,
   Finished,
@@ -136,7 +137,13 @@ class ProjectList {
     // listのconstructorが呼び出されるたびに、addListenerが実行される
     // 引数のcallback関数はjobとして配列に格納される
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -148,6 +155,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
