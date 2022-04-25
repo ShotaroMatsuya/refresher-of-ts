7ba8a3f7ab7df91514dd6aa4465ab849275d6195
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-26 00:49:04 +0900

    set starting point

---
+++ lib-test/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    port: 3000,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new Dotenv()],
+};
