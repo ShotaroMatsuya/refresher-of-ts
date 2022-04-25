6383b8ffe6c2c154ff6c27c5c42c3f292b9ac93e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-26 00:39:52 +0900

    using ES modules

--- drag-drop/src/models/project.ts
+++ drag-drop/src/models/project.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // Project Type
-  //グローバル定数として使える
-  export enum ProjectStatus {
-    Active,
-    Finished,
-  }
+// Project Type
+//グローバル定数として使える
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
 
-  export class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) {}
-  }
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
 }
