d1b649b3957612169ac0d425e1081c325fc8aaf8
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-26 10:26:54 +0900

    work with props and types

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import './App.css';
+
+import TodoList from './components/TotoList';
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const todos = [{ id: 't1', text: 'Finish the course' }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos}></TodoList>
+    </div>
+  );
 };
 
 export default App;
