5fd46ff4c4570d2008d9b974667e46a38107ac9f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-26 12:31:36 +0900

    create controller & add middleware of json-parser

--- app/src/routes/todos.ts
+++ app/src/routes/todos.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { createTodo } from '../controllers/todos';
 
 const router = Router();
 
-router.post('/');
+router.post('/', createTodo);
+
 router.get('/');
+
 router.patch('/:id');
+
 router.delete('/:id');
 
 export default router;
