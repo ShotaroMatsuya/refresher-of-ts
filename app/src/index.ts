e33d7a2f94fac809879e3bda5c7cb4ed36fd926f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-30 08:33:14 +0900

    set starting point

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,12 +1,5 @@
-/// <reference types="@types/google.maps" />
+console.log('hi there');
 
-import { User } from './User';
-import { Company } from './Company';
-import { CustomMap } from './CustomMap';
-
-const user = new User();
-const company = new Company();
-const customMap = new CustomMap('map');
-
-customMap.addMarker(user);
-customMap.addMarker(company);
+const logSomething = () => {
+  console.log('something');
+};
