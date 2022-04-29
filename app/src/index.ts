241d68457792ee30345a95f03e9c49b82dc3561c
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-29 18:25:27 +0900

    add Company Class

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+console.log(company);
