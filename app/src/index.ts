821df5e7022b7f252ba15484d7e1818a4a37447e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-01 07:28:48 +0900

    implement composition-based approach

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,8 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // Create an obj that satisfied the 'DataReader' interface
 
@@ -8,17 +10,11 @@ const csvFileReader = new CsvFileReader('football.csv');
 
 // Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
-
 matchReader.load();
-// matchReader.matches
 
-let manUnitedWins = 0;
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-console.log(`man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
