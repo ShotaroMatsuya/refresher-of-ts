84d7afbee9e20addbe4ed6cf5a9842afc4e3c529
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-04-30 14:30:31 +0900

    implement LinkedList sort

---
+++ app/src/LinkedList.ts
@@ -0,0 +1,87 @@
+class Node {
+  next: Node | null = null;
+
+  constructor(public data: number) {}
+}
+
+export class LinkedList {
+  // list内の最初のnodeを指すpointer
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    // node空のとき
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    //末尾までポインターを移動
+    while (tail.next) {
+      tail = tail.next;
+    }
+    // 最後にnodeを追加
+    tail.next = node;
+  }
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw Error('List is empty');
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
