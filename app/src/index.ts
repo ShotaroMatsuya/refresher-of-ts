3c8872a4e28d4a82f8047a05db03da1857e89340
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-01 15:32:00 +0900

    implement trigger events

--- app/src/index.ts
+++ app/src/index.ts
@@ -4,6 +4,15 @@ const user = new User({ name: 'myname', age: 20 });
 
 user.set({ name: 'newname', age: 9999 });
 
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('save was triggered');
+});
 
-console.log(user);
+user.trigger('change');
+// user.trigger('save');
