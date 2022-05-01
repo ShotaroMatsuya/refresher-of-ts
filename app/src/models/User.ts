389752fee355b70096b3920142ba787da5010e6a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-01 19:55:29 +0900

    integrate attributes

--- app/src/models/User.ts
+++ app/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -12,14 +13,9 @@ export class User {
   public sync: Sync<UserProps> = new Sync<UserProps>(
     'http://localhost:3030/users'
   );
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get<K extends keyof UserProps>(propName: K): UserProps[K] {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
