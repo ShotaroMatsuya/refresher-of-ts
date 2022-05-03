5245abe2f728c9fcd19317b907695c17c91f1304
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-04 03:54:45 +0900

    convert into generic class

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,10 @@
+import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
 
-const collection = new Collection('http://localhost:3030/users');
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3030/users',
+  (json: UserProps) => User.buildUser(json)
+);
 
 let timer = setTimeout(() => {
   collection.on('change', () => {
