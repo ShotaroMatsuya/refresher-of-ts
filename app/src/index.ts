8e871d170e9deb55b10970a2893436ee1034bf98
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-04 14:17:31 +0900

    read iput text

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,7 +3,7 @@ import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const formEl = document.getElementById('root')! as HTMLFormElement;
+const formEl = document.getElementById('root')!;
 const userForm = new UserForm(formEl, user);
 userForm.render();
 
