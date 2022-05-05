88f49bcb2a79ab3b37833b9acc1746773c44e9e0
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-05 15:05:14 +0900

    define route-binder

--- app/src/controllers/decorators/routes.ts
+++ app/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method: string) {
+  return function get(path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+export const get = routeBinder('get'); /*return decorator factory */
+export const put = routeBinder('put'); /*return decorator factory */
+export const post = routeBinder('post'); /*return decorator factory */
+export const del = routeBinder('del'); /*return decorator factory */
+export const patch = routeBinder('patch'); /*return decorator factory */
