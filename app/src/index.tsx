11055a18a480858abf8ffbcbbe5bc8f1d92be69d
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-05-07 16:54:31 +0900

    Revert "refactor to functional cmp"
    This reverts commit 6bbc1e5e0539dca9e9bcf1d08ec78efe8095006c.

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -5,15 +5,31 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
 
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App color="red" />
+    <App />
   </React.StrictMode>
 );
